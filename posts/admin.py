ce03d5a658677f1816c7babd18aac4abe9140770
Gustavo Silva <guhenriquels@gmail.com>
2022-09-15 13:26:48 -0300

    Added Posts in Models

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Posts
+
+
+@admin.register(Posts)
+class PostsAdmin(admin.ModelAdmin):
+    ...
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Posts(models.Model):
+    nome = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True)
+    cover = models.ImageField(upload_to='posts/%Y/%m-%d/', blank=False)
+    description = models.CharField(max_length=60)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return str(self.pk)
