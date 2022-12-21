01bdad64768797cb40137c15839af1b17adafcb8
Gustavo Silva <guhenriquels@gmail.com>
2022-12-21 16:13:44 -0300

    Updated request_img

--- posts/utils/request_img.py
+++ posts/utils/request_img.py
@@ -1,20 +1,23 @@
 import shutil  # save img locally
+from pathlib import Path
 
 import requests  # request img from web
+
+root_path = Path(__file__).parent.parent.parent
 
 
 def get_img(url, file_name):
     res = requests.get(url, stream=True)
 
     if res.status_code == 200:
-        with open(f'posts/temp/{file_name}', 'wb') as f:
+        with open(f'{root_path}/temp/{file_name}', 'wb') as f:
             res.raw.decode_content = True
             shutil.copyfileobj(res.raw, f)
         print('Image sucessfully Downloaded: ', file_name)
+
+        return f'{root_path}/temp/{file_name}'
     else:
         print('Image Couldn\'t be retrieved')
-
-    return f'posts/temp/{file_name}'
 
 
 if __name__ == '__main__':
